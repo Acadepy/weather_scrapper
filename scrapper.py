50631805bf83201e711bde0370fdd2446fd1fb26
Nestor <acadepython@gmail.com>
2022-02-14 20:23:18 +0100

    Data collected

--- scrapper.py
+++ scrapper.py
@@ -3,19 +3,16 @@ from bs4 import BeautifulSoup
 
 
 today = datetime.datetime.now()
-year_i = today.year
-year_f = today.year
-month_i = today.strftime("%m")
-month_f = today.strftime("%m")
-day_i = today.day
-day_f = today.day
+YEAR = today.year
+MONTH = today.strftime("%m")
+DAY = today.day
 headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'
         }
         
 if __name__ == '__main__':
-    url = f"https://www.wunderground.com/dashboard/pws/ITURCI5/table/{year_i}-{month_i}-{day_i}/{year_f}-{month_f}-{day_f}/daily"
-    print(f"Obteniendo datos desde el {day_i}/{month_i}/{year_i} al {day_f}/{month_f}/{year_f}")
+    url = f"https://www.wunderground.com/dashboard/pws/ITURCI5/table/{YEAR}-{MONTH}-{DAY}/{YEAR}-{MONTH}-{DAY}/daily"
+    print(f"Obteniendo datos del {DAY}/{MONTH}/{YEAR}")
 
     request = requests.get(url, headers=headers)
     soup = BeautifulSoup(request.text, 'lxml')
@@ -40,6 +37,8 @@ if __name__ == '__main__':
         
 
     for row in data_rows:
-        hour = datetime.datetime.strptime(row[0], "%I:%M %p") 
-        #Faltaría seleccionar el día. Ahora mismo está leyendo una hora pero el día es el 01/01/1900
-        print(hour)   
+        date_string = f"{DAY}/{MONTH}/{YEAR} " + row[0]
+        hour = datetime.datetime.strptime(date_string, "%d/%m/%Y %I:%M %p")
+        borrar = [data.split()[0] for data in row[1:]]
+        borrar.insert(0, hour.strftime("%d/%m/%Y %I:%M %p"))
+        print(borrar)
