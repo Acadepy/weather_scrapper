eb82a573f0685d5f6a34a7a2f0e3ab19f26e88fc
Nestor <acadepython@gmail.com>
2022-02-24 19:50:04 +0100

    Menu funcional con dos opciones. Un dia y varios dias.

--- scrapper.py
+++ scrapper.py
@@ -1,5 +1,4 @@
-from ntpath import join
-import requests, datetime
+import requests, datetime, time
 from bs4 import BeautifulSoup
 
 
@@ -23,7 +22,7 @@ def data_scraped(station=STATION, day=DAY, month=MONTH, year=YEAR):
     '''Devuelve todos los datos de un día concreto para una estación dada.'''
 
     url = f"https://www.wunderground.com/dashboard/pws/{station}/table/{year}-{month}-{day}/{year}-{month}-{day}/daily"
-    print(f"Estación: {STATION}.")
+    print(f"Estación: {station}.")
     print(f"Obteniendo datos del {day}/{month}/{year}.")
 
     request = requests.get(url, headers=headers)
@@ -42,6 +41,8 @@ def data_scraped(station=STATION, day=DAY, month=MONTH, year=YEAR):
     #Find all rows from the table and convert them to text
     table_body = table.find("tbody")
     table_rows = table_body.find_all("tr")
+    if not table_rows:
+        raise Exception('No hay datos en el día indicado.')
     data_rows = []
     for row in table_rows:
         columns_list = row.find_all("td")
@@ -79,17 +80,56 @@ def menu():
     station_to_scrape = input('Introduce el nombre de la estación: ')
     station_to_scrape = station_to_scrape if station_to_scrape else STATION
 
-    try:
-        day_to_scrape = datetime.datetime.strptime(input('Introduce el día para obtener los datos (dd/mm/aaaa): '), "%d/%m/%Y")
-    except:
-        print("Ha habido un error con la fecha. No tiene el formato correcto.")
+    option = input(f'Para datos de un dia pulsa 1, para datos de varios dias pulsa 2.')
+
+    if option == '1':
+        try:
+            day_to_scrape = datetime.datetime.strptime(input('Introduce el día para obtener los datos (dd/mm/aaaa): '), "%d/%m/%Y")
+        except:
+            print("Ha habido un error con la fecha. No tiene el formato correcto.")
+            return
+        obtener_un_dia(station_to_scrape, day_to_scrape)
+        
+    elif option == '2':
+        try:
+            day_to_scrape_i = datetime.datetime.strptime(input('Datos desde (dd/mm/aaaa): '), "%d/%m/%Y")
+            day_to_scrape_f = datetime.datetime.strptime(input('Datos hasta (dd/mm/aaaa): '), "%d/%m/%Y")
+        except:
+            print("Ha habido un error con la fecha. No tiene el formato correcto.")
+            return
+            
+        if day_to_scrape_f > today or day_to_scrape_i > today:
+            print("Alguna fecha es superior a hoy")
+            return
+        
+        
+        delta = day_to_scrape_f - day_to_scrape_i
+        delta_int = delta.days * (-1) if delta.days < 0 else delta.days
+        day_to_scrape = day_to_scrape_i if day_to_scrape_i < day_to_scrape_f else day_to_scrape_f
+
+        for i in range(delta_int):
+            obtener_un_dia(station_to_scrape, day_to_scrape)
+            print(f'Obtenidos datos de {day_to_scrape}')
+            day_to_scrape = day_to_scrape + datetime.timedelta(days=1)
+            print('Esperando tiempo entre consultas, 5 segundos...')
+            time.sleep(5)
+            
+
+    else:
+        print('Ninguna opción válida.')
         return
+
+
+    return station_to_scrape, day_to_scrape
     
+def obtener_un_dia(station_to_ob, day_to_ob):
+
     try:
-        data = data_scraped(station=station_to_scrape, day=day_to_scrape.day, month=day_to_scrape.month, year=day_to_scrape.year)
+        data = data_scraped(station=station_to_ob, day=day_to_ob.day, month=day_to_ob.month, year=day_to_ob.year)
         print("Se han obtenido correctamente los datos.")
-    except:
+    except Exception as exc:
         print("Ha habido un error al obtener los datos.")
+        print(exc.args)
         return
     
     try:
@@ -100,7 +140,7 @@ def menu():
         return
 
 def main():
-    pass
+    menu()
             
 if __name__ == '__main__':
     main()
